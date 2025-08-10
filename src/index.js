7f1f56a2d03a76cdee538d4598d43ac58ea0a405
pacureok <okkpacur@gmail.com>
2025-08-09 19:34:58 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Importa el componente principal
+import App from './App'; // Importa el componente principal de la app
 
+// Crea una raíz de React y la vincula al elemento con id="root"
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderiza el componente principal de la app
 root.render(
   <React.StrictMode>
     <App />
