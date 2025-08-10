1eaf3a20208998a909d6464e2a80b6df64dfaf41
pacureok <okkpacur@gmail.com>
2025-08-09 19:32:36 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Importa el componente principal
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
